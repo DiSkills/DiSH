fe890d889f292a63cafac131ebf4c1ecb548acbc
diskills <anti.python.projects@gmail.com>
2024-08-13 12:26:12 +0300

    Added wordlist

--- src/wordlist.c
+++ src/wordlist.c
@@ -1,1 +1,39 @@
+#include <stdlib.h>
+#include <string.h>
+
 #include "wordlist.h"
+
+
+void wordlist_init(struct wordlist_t *wordlist)
+{
+    wordlist->first = NULL;
+    wordlist->last = NULL;
+}
+
+
+void wordlist_push_back(struct wordlist_t *wordlist, const char *word)
+{
+    struct wordlist_item *tmp = malloc(sizeof(struct wordlist_item));
+    tmp->word = malloc(strlen(word) + 1);
+    strcpy(tmp->word, word);
+    tmp->next = NULL;
+
+    if (wordlist->last) {
+        wordlist->last->next = tmp;
+    } else {
+        wordlist->first = tmp;
+    }
+    wordlist->last = tmp;
+}
+
+
+void wordlist_del(struct wordlist_t *wordlist)
+{
+    while (wordlist->first) {
+        struct wordlist_item *tmp = wordlist->first;
+        wordlist->first = tmp->next;
+        free(tmp->word);
+        free(tmp);
+    }
+    wordlist->last = NULL;
+}
