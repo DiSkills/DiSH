e46fa95e17aecb9b3f63413a13547f6d4b19befb
diskills <anti.python.projects@gmail.com>
2024-09-05 17:22:57 +0300

    Added cmd.c: test_del

--- test/unit/cmd.c
+++ test/unit/cmd.c
@@ -15,6 +15,26 @@ void tearDown()
 }
 
 
+static void fill_cmd(struct cmd_t *cmd, const char *s,
+        enum cmd_states state, int pid, int code)
+{
+    int i;
+    struct line_t line;
+
+    line_init(&line);
+    for (i = 0; s[i]; i++) {
+        line_process_char(&line, s[i]);
+    }
+
+    line_to_cmd(&line, cmd);
+    cmd->state = state;
+    cmd->pid = pid;
+    cmd->code = code;
+
+    line_del(&line);
+}
+
+
 static void test_init()
 {
     struct cmd_t cmd;
@@ -28,6 +48,22 @@ static void test_init()
     TEST_ASSERT_EQUAL_INT(0, cmd.code);
 
     cmd_del(&cmd);
+}
+
+
+static void test_del()
+{
+    struct cmd_t cmd;
+    cmd_init(&cmd);
+    fill_cmd(&cmd, "ls -l\n", state_terminated, 2, 3);
+
+    cmd_del(&cmd);
+    TEST_ASSERT_NULL(cmd.name);
+    TEST_ASSERT_EQUAL_INT(0, cmd.argc);
+    TEST_ASSERT_NULL(cmd.argv);
+    TEST_ASSERT_EQUAL_INT(state_not_launched, cmd.state);
+    TEST_ASSERT_EQUAL_INT(0, cmd.pid);
+    TEST_ASSERT_EQUAL_INT(0, cmd.code);
 }
 
 
@@ -66,6 +102,7 @@ int main()
     UNITY_BEGIN();
 
     RUN_TEST(test_init);
+    RUN_TEST(test_del);
     RUN_TEST(test_line_to_cmd);
     
     return UNITY_END();
