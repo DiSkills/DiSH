e6c979297c66a04cce872a7a1bf31fe93718c783
diskills <anti.python.projects@gmail.com>
2024-09-08 19:02:52 +0300

    Added cmd_t

--- src/cmd.h
+++ src/cmd.h
@@ -1,3 +1,23 @@
 #ifndef CMD_H
 #define CMD_H
+
+enum cmd_states {
+    state_not_launched = 0,
+    state_launched = 1,
+    state_exited = 2,
+    state_terminated = 3
+};
+
+struct cmd_t {
+    char *name;
+
+    int argc;
+    char **argv;
+
+    enum cmd_states state;
+
+    int pid,
+        code;
+};
+
 #endif
---
+++ test/unit/cmd.c
@@ -0,0 +1,21 @@
+#include <unity.h>
+
+#include "src/cmd.h"
+
+
+void setUp()
+{
+}
+
+
+void tearDown()
+{
+}
+
+
+int main()
+{
+    UNITY_BEGIN();
+
+    return UNITY_END();
+}
