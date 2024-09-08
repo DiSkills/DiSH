82361a00d2cb45880ba6294f688c7d9056a65e25
diskills <anti.python.projects@gmail.com>
2024-09-08 11:09:38 +0300

    Added cmd_init

--- src/cmd.c
+++ src/cmd.c
@@ -1,1 +1,23 @@
+#include <stdlib.h>
+
 #include "cmd.h"
+
+
+static void cmd_fill_by_default(struct cmd_t *cmd)
+{
+    cmd->name = NULL;
+
+    cmd->argc = 0;
+    cmd->argv = NULL;
+
+    cmd->state = state_not_launched;
+
+    cmd->pid = 0;
+    cmd->code = 0;
+}
+
+
+void cmd_init(struct cmd_t *cmd)
+{
+    cmd_fill_by_default(cmd);
+}
