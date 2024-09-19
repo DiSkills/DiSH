24190a967957c5d0acfba6a4d7bf2348bfbe355c
DiSkills <anti.python.fastapi@gmail.com>
2024-09-19 18:36:36 +0300

    Added test_cmd_del

--- test/unit/cmd.c
+++ test/unit/cmd.c
@@ -15,6 +15,22 @@ void tearDown()
 }
 
 
+static void fill_cmd(struct cmd_t *cmd, const char *s, enum cmd_states state)
+{
+    int i;
+    struct line_t line;
+
+    line_init(&line);
+    for (i = 0; s[i]; i++) {
+        line_process_char(&line, s[i]);
+    }
+    cmd_init_from_line(cmd, &line);
+    cmd->state = state;
+
+    line_del(&line);
+}
+
+
 static void test_init_from_line()
 {
     int i;
@@ -30,7 +46,7 @@ static void test_init_from_line()
     cmd_init_from_line(&cmd, &line);
     TEST_ASSERT_EQUAL_STRING("ls", cmd.name);
 
-    TEST_ASSERT_EQUAL(4, cmd.argc);
+    TEST_ASSERT_EQUAL_INT(4, cmd.argc);
     TEST_ASSERT_EQUAL_STRING("ls", cmd.argv[0]);
     TEST_ASSERT_EQUAL_STRING("-l", cmd.argv[1]);
     TEST_ASSERT_EQUAL_STRING("-a", cmd.argv[2]);
@@ -39,14 +55,27 @@ static void test_init_from_line()
 
     TEST_ASSERT_EQUAL_PTR(cmd.argv[0], cmd.name);
 
-    TEST_ASSERT_EQUAL(state_not_launched, cmd.state);
+    TEST_ASSERT_EQUAL_INT(state_not_launched, cmd.state);
 
     TEST_ASSERT_LINE(line, 0, 0, 0, noerror, mode_split);
     TEST_ASSERT_LINE_WORD_IS_DEFAULT(line);
     TEST_ASSERT_LINE_WORDLIST_IS_EMPTY(line);
 
     line_del(&line);
-    /* TODO cmd_del(&cmd); */
+    cmd_del(&cmd);
+}
+
+
+static void test_del()
+{
+    struct cmd_t cmd;
+    fill_cmd(&cmd, "ls -l\n", state_terminated);
+
+    cmd_del(&cmd);
+    TEST_ASSERT_NULL(cmd.name);
+    TEST_ASSERT_EQUAL_INT(0, cmd.argc);
+    TEST_ASSERT_NULL(cmd.argv);
+    TEST_ASSERT_EQUAL_INT(state_not_launched, cmd.state);
 }
 
 
@@ -55,6 +84,7 @@ int main()
     UNITY_BEGIN();
 
     RUN_TEST(test_init_from_line);
+    RUN_TEST(test_del);
 
     return UNITY_END();
 }
