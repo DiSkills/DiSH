14fa9a6b3cf8eaef231f3ae097f6b54d4019f055
Arkady Sokolenko <github@diskills-developer.org>
2025-11-07 20:25:40 +0300

    refactor lexer

--- include/lexer.hpp
+++ include/lexer.hpp
@@ -5,42 +5,39 @@
 
 #include "string_buffer.hpp"
 
-enum {
-    BACKGROUND = '&',
-    SEQUENCE = ';',
+namespace Tokens {
 
-    INPUT_REDIRECTION = '<',
-    OUTPUT_REDIRECTION = '>',
-    APPEND_REDIRECTION = 256,
-
-    PIPE = '|',
-
-    LBRACKET = '(',
-    RBRACKET = ')',
-
-    AND = 257,
-    OR = 258,
-
-    WORD = 259,
+enum Tag {
+    BACKGROUND, SEQUENCE,
+    INPUT_REDIRECTION, OUTPUT_REDIRECTION, APPEND_REDIRECTION,
+    PIPE,
+    LBRACKET, RBRACKET,
+    AND, OR,
+    WORD,
+    NEWLINE, END,
 };
 
 class Token {
-    int tag;
+    Tag tag;
 public:
-    Token(int tag) : tag(tag) {}
+    explicit Token(Tag tag) : tag(tag) {}
     virtual ~Token() {}
-    int GetTag() const { return tag; }
+
+    Tag GetTag() const { return tag; }
 };
 
 class Word : public Token {
     char *lexeme;
 public:
-    Word(char *lexeme) : Token(WORD), lexeme(lexeme) {}
+    explicit Word(char *lexeme) : Token(WORD), lexeme(lexeme) {}
     virtual ~Word() { delete[] lexeme; }
-    const char *GetLexeme() const { return lexeme; }
+
+    char *GetLexeme() const { return lexeme; }
 private:
     Word(const Word &);
     void operator=(const Word &);
+};
+
 };
 
 class Lexer {
@@ -48,17 +45,17 @@ class Lexer {
     StringBuffer lexeme;
 public:
     Lexer() : peek(' ') {}
-    Token *Scan();
+    Tokens::Token *Scan();
 private:
     void GetChar() { peek = getchar(); }
     bool GetChar(char c)
         { GetChar(); if (peek != c) return false; ResetPeek(); return true; }
     void ResetPeek() { peek = ' '; }
 
-    Token *GetOperatorToken() const;
+    Tokens::Token *GetOperatorToken() const;
     void SkipSpaces() { while (IsSpace(peek)) GetChar(); }
-    Word *ScanLexeme();
-    Token *ScanOperator();
+    Tokens::Word *ScanLexeme();
+    Tokens::Token *ScanOperator();
 
     static bool IsSpace(char c) { return c == ' ' || c == '\t'; }
     static bool IsSpecialChar(char c);
